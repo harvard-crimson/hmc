40a6b5a49832d03d7c50cad6bfaaf12565363214
alexhpatel <alexanderpatel@college.harvard.edu>
2014-05-29 03:40:19 -0400

    refactor files

--- graphics/Change article   thecrimson.com site admin_files/media_include.js
+++
@@ -1,9 +0,0 @@
-var media_list = new Array();
-
-
-function require_once(url) {
-    if(typeof(media_list[url]) == 'undefined'){
-        media_list[url]=true;
-        document.write('<script type="text/javascript" src="' + url + '"></script>');
-    }
-}
--- graphics/Change article   thecrimson.com site admin_files/noenter.js
+++
@@ -1,9 +0,0 @@
-//Function to eat up the enters that people put into forms so it doesn't
-//automatically submit.
-function stopRKey(evt) {
-  var evt = (evt) ? evt : ((event) ? event : null);
-  var node = (evt.target) ? evt.target : ((evt.srcElement) ? evt.srcElement : null);
-  if ((evt.keyCode == 13) && (node.type=="text"))  {return false;}
-}
-
-document.onkeypress = stopRKey; 
